3eacabcc0a14967fa746b3918ab69fe839c5220a
un3th1c4l-qu3lls <muramoto.masumi@gmail.com>
2026-06-01 22:54:50 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -149,6 +149,7 @@ pub mod option {
     }
 
     pub fn make(option: (u8, &[u8])) -> Result<(), &'static str> {
+        //! Mirrors option::check() : this was not intended (an error can be considered false).
         if option.1.len() > 0xfe {
             return Err("`option` too big.");
         } else if matches!(option.0, self::eol::KIND | self::nop::KIND) && option.1.len() != 0 {
@@ -158,16 +159,17 @@ pub mod option {
     }
 
     pub fn check(option: (u8, &[u8])) -> bool {
-        !(option.1.len() > 0xfe || (matches!(option.0, self::eol::KIND | self::nop::KIND) && option.1.len() != 0))
+        !(option.1.len() > 0xfe
+            || (matches!(option.0, self::eol::KIND | self::nop::KIND) && option.1.len() != 0))
     }
 
     pub mod eol {
         //! End of Option List
         pub const KIND: u8 = 0;
-        pub fn from_option(tuple: (u8, &[u8])) -> Result<(), &'static str> {
-            if tuple.0 != self::KIND {
+        pub fn from_option(option: (u8, &[u8])) -> Result<(), &'static str> {
+            if option.0 != self::KIND {
                 return Err("`eol` kind mismatch.");
-            } else if tuple.1.len() != 0 {
+            } else if option.1.len() != 0 {
                 return Err("`eol` expected no data.");
             }
             Ok(())
@@ -181,10 +183,10 @@ pub mod option {
     pub mod nop {
         //! No Operation
         pub const KIND: u8 = 1;
-        pub fn from_option(tuple: (u8, &[u8])) -> Result<(), &'static str> {
-            if tuple.0 != self::KIND {
+        pub fn from_option(option: (u8, &[u8])) -> Result<(), &'static str> {
+            if option.0 != self::KIND {
                 return Err("`nop` kind mismatch.");
-            } else if tuple.1.len() != 0 {
+            } else if option.1.len() != 0 {
                 return Err("`nop` expected no data.");
             }
             Ok(())
@@ -198,17 +200,172 @@ pub mod option {
     pub mod mss {
         //! Maximum Segment Size
         pub const KIND: u8 = 2;
-        pub fn from_option(tuple: (u8, &[u8])) -> Result<u32, &'static str> {
-            if tuple.0 != self::KIND {
+        pub fn from_option(option: (u8, &[u8])) -> Result<u32, &'static str> {
+            if option.0 != self::KIND {
                 return Err("`mss` kind mismatch.");
-            } else if tuple.1.len() != 4 {
+            } else if option.1.len() != 4 {
                 return Err("`mss` expected exactly 4 bytes.");
             }
-            Ok(u32::from_be_bytes(tuple.1[..4].try_into().unwrap()))
+            Ok(u32::from_be_bytes(option.1[..4].try_into().unwrap()))
         }
 
         pub fn to_option(mss: u32) -> Result<(u8, Vec<u8>), &'static str> {
             Ok((self::KIND, mss.to_be_bytes().to_vec()))
+        }
+    }
+
+    pub mod window_scale {
+        //! Window Scale
+        pub const KIND: u8 = 3;
+        pub fn from_option(option: (u8, &[u8])) -> Result<u8, &'static str> {
+            if option.0 != self::KIND {
+                return Err("`window_scale` kind mismatch.");
+            } else if option.1.len() != 1 {
+                return Err("`window_scale` expected exactly 1 bytes.");
+            }
+            Ok(option.1[0])
+        }
+
+        pub fn to_option(shift_count: u8) -> Result<(u8, Vec<u8>), &'static str> {
+            Ok((self::KIND, [shift_count].to_vec()))
+        }
+    }
+
+    pub mod sack_permitted {
+        //! SACK-Permitted
+        pub const KIND: u8 = 4;
+        pub fn from_option(option: (u8, &[u8])) -> Result<(), &'static str> {
+            if option.0 != self::KIND {
+                return Err("`sack_permitted` kind mismatch.");
+            } else if option.1.len() != 0 {
+                return Err("`sack_permitted` expected exactly 0 bytes.");
+            }
+            Ok(())
+        }
+
+        pub fn to_option() -> Result<(u8, &'static [u8]), &'static str> {
+            Ok((self::KIND, &[]))
+        }
+    }
+
+    pub mod sack { // Not clear -> format, clarify
+        //! SACK ==> needs to be done
+        pub const KIND: u8 = 5;
+        pub fn from_option(option: (u8, &[u8])) -> Result<Vec<(u32, u32)>, &'static str> {
+            if option.0 != self::KIND {
+                return Err("`sack` kind mismatch.");
+            } else if option.1.len() % 8 != 0 {
+                return Err("`sack` expected pairs of 4 byte values.");
+            }
+            let mut block_edges = Vec::<(u32, u32)>::with_capacity(option.1.len() / 8);
+            for i in 0..option.1.len() / 8 {
+                block_edges.push((u32::from_be_bytes(option.1[8 * i + 0..8 * i + 4].try_into().unwrap()), u32::from_be_bytes(option.1[8 * i + 4..8 * i + 8].try_into().unwrap())))
+            }
+            Ok(block_edges)
+        }
+
+        pub fn to_option(block_edges: Vec<(u32, u32)>) -> Result<(u8, Vec<u8>), &'static str> {
+            let mut bytes = Vec::<u8>::with_capacity(block_edges.len() * 8);
+            for pair in &block_edges {
+                bytes.extend_from_slice(&(((pair.0 as u64) << 32) | (pair.1 as u64)).to_be_bytes());
+            }
+            
+            Ok((self::KIND, bytes))
+        }
+    }
+
+    pub mod timestamps {
+        //! Timestamps
+        pub const KIND: u8 = 8;
+        pub fn from_option(option: (u8, &[u8])) -> Result<(u32, u32), &'static str> {
+            if option.0 != self::KIND {
+                return Err("`timestamps` kind mismatch.");
+            } else if option.1.len() != 8 {
+                return Err("`timestamps` expected exactly 8 bytes.");
+            }
+            Ok((u32::from_be_bytes(option.1[..4].try_into().unwrap()), u32::from_be_bytes(option.1[4..8].try_into().unwrap())))
+        }
+
+        pub fn to_option(tuple: (u32, u32)) -> Result<(u8, Vec<u8>), &'static str> {
+            let mut bytes = Vec::<u8>::with_capacity(8);
+            bytes.extend_from_slice(&tuple.0.to_be_bytes());
+            bytes.extend_from_slice(&tuple.1.to_be_bytes());
+            Ok((self::KIND, bytes))
+        }
+    }
+
+    pub mod user_timeout {
+        //! TCP User Timeout
+        pub const KIND: u8 = 28;
+        pub fn from_option(option: (u8, &[u8])) -> Result<(bool, u16), &'static str> {
+            if option.0 != self::KIND {
+                return Err("`user_timeout` kind mismatch.");
+            } else if option.1.len() != 2 {
+                return Err("`user_timeout` expected exactly 2 bytes.");
+            }
+            let value = u16::from_be_bytes(option.1[..2].try_into().unwrap());
+            Ok((value >> 15 == 1, value & 0x7fff))
+        }
+
+        pub fn to_option(tuple: (bool, u16)) -> Result<(u8, Vec<u8>), &'static str> {
+            if tuple.1 > 0x7fff {
+                return Err("`user_timeout` expected 15 bit unsigned integer.")
+            }
+            Ok((self::KIND, ((if tuple.0 {1 << 15} else {0}) | tuple.1).to_be_bytes().to_vec()))
+        }
+    }
+
+    pub mod ao {
+        //! TCP Authentication
+        pub const KIND: u8 = 29;
+        pub fn from_option<'a>(option: (u8, &'a[u8])) -> Result<(u8, u8, &'a[u8]), &'static str> {
+            if option.0 != self::KIND {
+                return Err("`ao` kind mismatch.");
+            } else if option.1.len() < 2 {
+                return Err("`ao` expected at least 2 bytes.");
+            }
+            Ok((option.1[0], option.1[1], &option.1[2..]))
+        }
+
+        pub fn to_option<'a>(tuple: (u8, u8, &'a[u8])) -> Result<(u8, Vec<u8>), &'static str> {
+            let mut bytes = Vec::<u8>::with_capacity(2 + tuple.2.len());
+            bytes.push(tuple.0);
+            bytes.push(tuple.1);
+            bytes.extend_from_slice(tuple.2);
+            Ok((self::KIND, bytes))
+        }
+
+        // When computing the mac, this option field must be set to 0.
+    }
+
+    pub mod fast_open {
+        //! TCP Fast Open
+        pub const KIND: u8 = 34;
+        pub fn from_option<'a>(option: (u8, &'a[u8])) -> Result<Option<&'a[u8]>, &'static str> {
+            if option.0 != self::KIND {
+                return Err("`fast_open` kind mismatch.");
+            }
+            match option.1.len() {
+                0 => Ok(None),
+                4 | 6 | 8 | 10 | 12 | 14 | 16 => Ok(Some(option.1)),
+                _ => Err("`fast_open` expected exactly 0 | 4 | 6 | 8 | 10 | 12 | 14 | 16 bytes."),
+            }
+        }
+
+        pub fn to_option<'a>(cookie: &'a[u8]) -> Result<(u8, Vec<u8>), &'static str> {
+            Ok((self::KIND, cookie.to_vec()))
+        }
+    }
+
+    pub mod experimental {
+        //! Experimentals
+        pub const KINDS: [u8; 3] = [76, 253, 254];
+        pub fn from_option(tuple: (u8, &[u8])) -> Result<(), &'static str> {
+            Err("Unavailable, format unknown.")
+        }
+
+        pub fn to_option() -> Result<(u8, &'static[u8]), &'static str> {
+            Err("Unavailable, format unknown.")
         }
     }
 }
@@ -251,6 +408,7 @@ pub mod segment {
         Ok(bytes)
     }
 }
+
 
 /*
 pub fn add(left: u64, right: u64) -> u64 {
